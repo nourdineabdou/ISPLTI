81f9ea5d39b392cdfd32aa302cd26767aaf7ecea
nourdine med souleymane <nourdine2177@gmail.com>
2025-08-21 15:21:45 +0000

    df

--- database/migrations/2025_08_04_153118_create_etudiants_table.php
+++ database/migrations/2025_08_04_153118_create_etudiants_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('etudiants', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->id();
             $table->string('nodos', 10);
             $table->string('nom_ar')->nullable();
             $table->string('nom_fr')->nullable();
