643c7241587c2228f2921a2b7f1faaecbaee4523
nourdine med souleymane <nourdine2177@gmail.com>
2025-09-26 01:27:10 +0000

    sldef

--- app/Http/Middleware/SlowDown.php
+++ app/Http/Middleware/SlowDown.php
@@ -13,8 +13,8 @@ class SlowDown
         //dd($request->is('/'));
         if ($request->is('/')) {
 
-            $min = (int) env('SLOW_MIN', 7);
-            $max = (int) env('SLOW_MAX', 10);
+            $min = (int) env('SLOW_MIN', 1);
+            $max = (int) env('SLOW_MAX', 2);
             sleep(random_int($min, $max)); // bloque le worker
              // pour test
         }
