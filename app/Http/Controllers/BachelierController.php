ef25e19bbf1824f3604586bf9aee7d17bf50f1af
nourdine med souleymane <nourdine2177@gmail.com>
2025-10-10 13:49:27 +0000

    jgfjgf

--- app/Http/Controllers/BachelierController.php
+++ app/Http/Controllers/BachelierController.php
@@ -112,14 +112,14 @@ public function getImage($id)
                             [
                                 'label' => 'Valider L\'inscription',
                                 'onclick' => 'confirmAction({ title: \'Confirmer la validation\', text: \'Voulez-vous vraiment valider l inscription de cet étudiant ?\', confirmButtonText: \'Oui, valider !\', url: \'' . route('bacheliers.valider', $bachelier->id) . '\', method: \'GET\' })',
-                                'permission' => $user->id == 8 ? false : true
+                                'permission' => $user->id == 9 ? false : true
                             ]
                             ,
                             // deque je rejeter le bachelier je veux qui mafiche visulier etudiant rejeter
                             [
                                 'label' => 'Rejeter L\'inscription',
                                 'onclick' => 'confirmAction({ title: \'Confirmer le rejet\', text: \'Voulez-vous vraiment rejeter l inscription de cet étudiant ?\', confirmButtonText: \'Oui, rejeter !\', url: \'' . route('bacheliers.rejeter', $bachelier->id) . '\', method: \'GET\' })',
-                                'permission' => $user->id == 8 ? false : true
+                                'permission' => $user->id == 9 ? false : true
                             ]
                         ];
                     }
@@ -134,7 +134,7 @@ public function getImage($id)
                             [
                                 'label' => 'Modifier le motif de rejet',
                                 'onclick' => 'openInModal({ link: \'' . route('bacheliers.edit', $bachelier->id) . '\', size: \'lg\' })',
-                                'permission' => $user->id == 8 ? false : true
+                                'permission' => $user->id == 9   ? false : true
                             ],
                         ];
                     }
