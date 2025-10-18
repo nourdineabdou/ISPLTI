2f590dd69e504f907e5b58cd1c8d95da39dbf474
nourdine med souleymane <nourdine2177@gmail.com>
2025-10-18 15:10:26 +0000

    g

--- resources/views/pages/etudiants/export-attestation.blade.php
+++ resources/views/pages/etudiants/export-attestation.blade.php
@@ -158,6 +158,7 @@
                                     </td>
                                 @endif
                                 <td style="font-size:7pt;">{{ $element['module'] ?? '' }}</td>
+                                @dd($element['element'])
                                 <td style="font-size:7pt;">{{  $element['matiere'] }} {{ $element['element']  }}</td>
                                 <td style="text-align:center; font-size:7pt;">{{ $element['volume_horaire'] ?? '' }}</td>
                                 <td style="text-align:center; font-size:7pt;">{{ $element['credits'] ?? '' }}</td>
